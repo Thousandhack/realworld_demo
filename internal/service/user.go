6bcb39f04c58e9fbd2c0b1907bb47c7f0bf098ef
Thousandhack <1391053697@qq.com>
2025-07-13 16:03:24 +0800

    fix:注册去掉多余的校验

--- internal/service/user.go
+++ internal/service/user.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	v1 "realworld_demo/api/realworld/v1"
 	"realworld_demo/internal/biz"
-
-	"github.com/go-kratos/kratos/v2/transport"
 )
 
 // Login 方法
@@ -26,21 +24,6 @@ func (s *RealWorldService) Login(ctx context.Context, req *v1.LoginRequest) (rep
 // Register 方法
 func (s *RealWorldService) Register(ctx context.Context, req *v1.RegisterRequest) (reply *v1.UserReply, err error) {
 	fmt.Println("\n\n=== 开始处理注册请求 ===")
-	fmt.Printf("请求内容: %+v\n", req)
-
-	// 打印传输信息
-	if tr, ok := transport.FromServerContext(ctx); ok {
-		fmt.Printf("请求类型: %s\n", tr.Kind())
-		fmt.Printf("请求操作: %s\n", tr.Operation())
-		fmt.Printf("请求头: %v\n", tr.RequestHeader())
-	}
-
-	// 参数验证
-	if req == nil {
-		fmt.Println("请求参数错误: 请求为空")
-		return nil, fmt.Errorf("请求参数错误: 请求为空")
-	}
-
 	if req.User == nil {
 		fmt.Println("请求参数错误: user为空")
 		return nil, fmt.Errorf("请求参数错误: user为空")
